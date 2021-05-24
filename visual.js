3f060fa645cb8b9c7991f630d721e6d6662b0490
jinsu <jinsu4205@gmail.com>
2021-05-24 19:37:05 +0900

    1

--- visual.js
+++ visual.js
@@ -1,7 +1,7 @@
 import { Text } from "./text.js";
 import { Particle } from "./particle.js";
 
-export const RANDOM_TEXT = "양지은";
+export const RANDOM_TEXT = "HELLO";
 
 export class Visual {
   constructor() {
